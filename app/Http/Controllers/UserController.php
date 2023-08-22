f651f05bf6f3b33ecdb0196a517970c7d2530534
ali jafari <jafari.8001.ali@gmail.com>
2023-08-22 23:43:28 +0330

    add delete, update and get function to User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Response;
 
@@ -24,5 +25,52 @@ class UserController extends Controller
                 "data"=> $create,
             ]
         );
+    }
+
+    public function get_user( $id = null){
+        if ($id == null) {
+            return Response()->json(User::all());
+        }
+        return Response()->json(User::where('id', $id)->get());
+    }
+
+    public function edit_user(Request $request, $id){
+        try {
+            $user = User::find($id);
+            $user->update($request->all());
+            return Response()->json([
+                'status'=> 201,
+                'message'=>'updating user successfull',
+                'data'=>''
+            ]);
+        } catch (Exception $err) {
+                return Response()->json([
+                'status'=> $err->getCode(),
+                'message'=>$err->getMessage(),
+                'data'=>''
+            ]);
+        }
+    }
+
+    public function delete_user($id){
+        $user = User::find($id)->delete();
+        if($user){
+            return Response()->json([
+                'status'=> 201,
+                'message'=> 'User removing seccessful',
+                'data'=> "",
+            ]);
+        }else {
+            return Response()->json([
+                'status'=> 400,
+                'message'=> 'User removing faild',
+                'data'=> "",
+            ]);
+        }
+    }
+
+    public function count_loan($id){
+        $user = User::find($id)->loans;
+        return Response()->json(count($user));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/get_user/{id?}', [UserController::class, 'get_user'] );
+Route::get('/count_loan/{id}', [UserController::class, 'count_loan'] );
 Route::post('/add_user', [UserController::class, 'add_user'] );
+Route::delete('/delete_user/{id}', [UserController::class, 'delete_user'] );
+Route::put('/update/{id}', [UserController::class, 'edit_user']);
