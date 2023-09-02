bb08d0f7d90c5ea092ff797ef257e55d4f5f63e0
ali jafari <jafari.8001.ali@gmail.com>
2023-09-02 23:21:18 +0330

    EDIT: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,27 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        User::create([
+        User::insertData([
             "first_name"=> "ali",
             "last_name"=> "jafari",
             "phone_number"=> "09933861217",
-            "password"=> "$2y$10$6d1rLQd0qCxpgpTKGFeN0ulMJ.J5Uw7ItJvzsJtMiCKWIImyR/1SK",
+            "password"=> "jafari8001@",
             "age"=> "22",
             "gender"=> "male",
         ]);
+        Role::insertData([
+            "name"=> "admin",
+        ]);
+        $user = User::findDataById(1);
+        $role = Role::find(1);
+        $user->roles()->save($role);
     }
 }
